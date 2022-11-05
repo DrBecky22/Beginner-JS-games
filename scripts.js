0ad7d45528ab39f8183c15d88ab498d469dcc647
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2022-11-04 23:58:49 -0500

    attempt to link HTML to JS
    Nov4 attempts to connect buttons "on click" to specific js functions written in js document

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
 const express = require ('express')
 const app = express()
 
-// Time Left Calculator
-function TimeCalculator (){
-var name = prompt ("What is your name");
-var ageYears = prompt ("Hi, " + name + "! If you tell me your age in years, I will tell you how many days, weeks and months left until your 90th birthday.  Please list your age in years below.");
-var yearsLeft = (90-ageYears);
-var days = (yearsLeft*365);
-var weeks = (yearsLeft*52);
-var months = (yearsLeft*12);
-alert ("Ok, " + name + " you have " + days + ", " + weeks + ", and " + months + " left before your 90th year. Spend it well, Live long and Prosper, Be excellent and party on."); 
+// Time Left Calculator //
+function timeCalculator(){
+    let name = prompt ("What is your name?");
+    let ageYears = prompt ("Hi, " + name + "! If you tell me your age in years, I will tell you how many days, weeks and months left until your 90th birthday.  Please list your age in years below.");
+    var yearsLeft = (90-ageYears);
+    var days = (yearsLeft*365);
+    var weeks = (yearsLeft*52);
+    var months = (yearsLeft*12);
+    alert ("Ok, " + name + " you have " + days + ", " + weeks + ", and " + months + " left before your 90th year. Spend it well, Live long and Prosper, Be excellent and party on."); 
 }
-    let btn = document.getElementById("TimeCalc-btn");
-    btn.addEventListener('click', event => {
-      TimeCalculator();
-    });
+    // let btn = document.getElementById("TimeCalc-btn");
+    // btn.addEventListener('click', event => {
+    //   TimeCalculator();
+    // });
 
-// Love Calculator
-function loveCalculator () {
+// Love Calculator  //
+function loveCalculator() {
 prompt ("what is your name");
 prompt ("what is your intended's name?");
 
-var loveScore = Math.random () *100;
+var loveScore = Math.random() *100;
 loveScore = Math.floor(loveScore) +1;
 
 if (loveScore >=70) {
